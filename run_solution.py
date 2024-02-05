c6f6cffc1cc083c61cacbc384660e5c155bc193f
faenwitee <157916054+faenwitee@users.noreply.github.com>
2024-02-05 01:27:18 -0500

    Update run_solution.py

--- run_solution.py
+++ run_solution.py
@@ -1,8 +1,8 @@
 import sys
 import subprocess
-# p = subprocess.getoutput("{} ./adventure.py < solution.txt".format(sys.executable))
-# print(p)
+p = subprocess.getoutput("{} ./adventure.py < solution.txt".format(sys.executable))
+print(p)
 # p1 = subprocess.getoutput("{} ./adventure.py < gameover.txt".format(sys.executable))
 # print(p1)
-p2 = subprocess.getoutput("{} ./adventure.py < gameplay1.txt".format(sys.executable))
-print(p2)
+# p2 = subprocess.getoutput("{} ./adventure.py < gameplay1.txt".format(sys.executable))
+# print(p2)
